84cfc078c04f8579381519a5f6198898d1125a7f
Tracy Kaur <tracykaur@yahoo.com>
2021-03-31 23:17:59 +0000

    changes for deployment

--- app/actions.rb
+++ app/actions.rb
@@ -8,3 +8,18 @@ get '/signup' do     # if a user navigates to the path "/signup",
   @user = User.new   # setup empty @user object
   erb(:signup)       # render "app/views/signup.erb"
 end
+
+post '/signup' do
+  email      = params[:email]
+  avatar_url = params[:avatar_url]
+  username   = params[:username]
+  password   = params[:password]
+
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+
+  if @user.save
+    "User #{username} saved!"
+  else
+    erb(:signup)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
     has_many :finstagram_posts
     has_many :comments
     has_many :likes
+
+    validates :email, :username, uniqueness: true
+  validates :email, :avatar_url, :username, :password, presence: true
 end
